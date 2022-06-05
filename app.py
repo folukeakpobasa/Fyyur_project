86589fe7df54a752a660bd4b246b10512971b86e
folukeakpobasa <folukeakpobasa@decagon.dev>
2022-06-05 22:15:29 +0100

    fix: db path

--- app.py
+++ app.py
@@ -39,9 +39,13 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
+    
+    # missing fields
+    # website_link = db.Column(db.String(150))
+    # looking_for_talent = db.Column.(db.Boolean)
+    # seeking_description = db.Column(db.String(500))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-# db.create_all()
+
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
